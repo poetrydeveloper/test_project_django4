2411cc253ae09cd581de9c1c2dc1669f54496ade
Marat Gumerov <gumerovm74@gmail.com>
2024-04-24 22:52:02 +0300

    added send email

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('<int:post_id>/share/',
+         views.post_share, name='post_share'),
 ]
